d71b8bb1d22103940f51a7ee800301e9ec4c6248
digitalchandan <digitalchandansingh@gmail.com>
2020-09-28 21:29:09 +0530

    Update 35-check-greatest-number.c

--- 35-check-greatest-number.c
+++ 35-check-greatest-number.c
@@ -1,44 +1,44 @@
 # include<stdio.h>
 
 int main(){
-    int a,b,c,d;
-    printf("Enter the value of a\n");
-    scanf("%d", &a);
+    int num1,num2,num3,num4;
+    printf("Enter the value of num1\n");
+    scanf("%d", &num1);
 
-    printf("Enter the value of b\n");
-    scanf("%d", &b);
+    printf("Enter the value of num2\n");
+    scanf("%d", &num2);
 
-    printf("Enter the value of c\n");
-    scanf("%d", &c);
+    printf("Enter the value of num3\n");
+    scanf("%d", &num3);
 
-    printf("Enter the value of d\n");
-    scanf("%d", &d);        
+    printf("Enter the value of num4\n");
+    scanf("%d", &num4);        
 
-    if(a>b){
-        if(a>c){
-            if(a>d){
-                printf("A is greater among 4 numbers\n");
+    if(num1>num2){
+        if(num1>num3){
+            if(num1>num4){
+                printf("Num1 is greater among 4 numbers\n");
             }  
         }
     }
-    if(b>a){
-        if (b>c){
-            if(b>d){
-                printf("B is greater among 4 numbers\n");
+    if(num2>num1){
+        if (num2>num3){
+            if(num2>num4){
+                printf("Num2 is greater among 4 numbers\n");
             }  
         }
     }
-    if (c>a){
-        if (c>b){
-            if(c>d){
-                printf("C is greater among 4 numbers\n");
+    if (num3>num1){
+        if (num3>num2){
+            if(num3>num3){
+                printf("Num3 is greater among 4 numbers\n");
             }
         }    
     }
-    if (d>a){
-        if(d>b){
-            if(d>c){
-                printf("D is greater among 4 numbers\n");
+    if (num4>num1){
+        if(num4>num2){
+            if(num4>num3){
+                printf("Num4 is greater among 4 numbers\n");
             }
         }
     }
