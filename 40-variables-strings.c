3ba459238c5d3e26eb8c9c4ba5fa7bdc7a308594
digitalchandan <digitalchandansingh@gmail.com>
2020-10-05 20:02:37 +0530

    Create 40-variables-strings.c

---
+++ 40-variables-strings.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+    char name[] = "Chandan";
+    int age = 18;
+    printf("My name is %s. \n", name);
+    printf("My age in 2019 was %d years. \n", age);
+
+    age = 19;
+    printf("Now my age in 2020 is %d years. \n", age);
+return 0;
+}
