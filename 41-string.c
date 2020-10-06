bed2437af955a9997efbdfe22340a5050241807e
chandancodes <chandacodes@gmail.com>
2020-10-06 19:05:29 +0530

    1

---
+++ 41-string.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(){
+    char name[] = "Chandan";
+    int age = 18;
+    printf("My name is %s \n", name);
+    printf("My age is %d years. \n", age);
+return 0;
+}
---
+++ 42-data-type.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(){
+    int a=2;
+    double b=3.5;
+    char grade='A'; // One single letter allowed.
+    float c=4.5;
+return 0;
+}
