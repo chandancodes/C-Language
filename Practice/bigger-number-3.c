73d313f692bfada362a6c057fe64a2573393a1a6
chandancodes <chandacodes@gmail.com>
2020-10-26 22:13:51 +0530

    1

--- Practice/bigger-number-3.c
+++ Practice/bigger-number-3.c
@@ -2,22 +2,22 @@
 
 int main(){
     int num1,num2,num3;
-    printf("Enter Num1 Value: %d \n", num1);
+    printf("Enter Num1 Value:\n");
     scanf("%d", &num1);
-    printf("Enter Num2 Value: %d \n", num2);
+    printf("Enter Num2 Value:\n");
     scanf("%d", &num2);
-    printf("Enter Num3 Value: %d \n", num3);
+    printf("Enter Num3 Value:\n");
     scanf("%d", &num3);
 
     if(num1>num2){
         if(num1>num3){
             printf("Num1 Is Greatest: %d \n", num1);
         }
-    }else if(num2>num1){
+    }if(num2>num1){
         if(num2>num3){
             printf("Num2 Is Greatest: %d \n", num2);
         }
-    }else if(num3>num1){
+    }if(num3>num1){
         if(num3>num2){
             printf("Num3 Is Greatest: %d \n", num3);
         }
